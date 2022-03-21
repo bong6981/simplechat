ee48fa3dbebfe21b7a8c8f617100dcef4243f173
bong6981 <kbk5190@naver.com>
2022-03-21 18:02:32 +0900

    Expose Stomp endpoints and set destinations prefix

---
+++ src/main/java/study/simplechat/stomp/config/StompWebSocketConfig.java
@@ -0,0 +1,23 @@
+package study.simplechat.stomp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Profile("stomp")
+@EnableWebSocketMessageBroker
+@Configuration
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp-chat").setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/topic");
+    }
+}
